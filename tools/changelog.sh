1410d1235def038aa32eac1ed96f2c39832ec1a6
Jack Summers <jacksummers715@gmail.com>
2020-04-11 10:12:42 -0400

    fix broken changelog gen

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -17,7 +17,8 @@ export Until_Date=`date --date="$k days ago" +%F`
 echo "====================" >> $Changelog;
 echo "     $Until_Date    " >> $Changelog;
 echo "====================" >> $Changelog;
-repo forall -pc 'git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7' >> $Changelog
+repo forall -c "GIT_LOG=\`git log --oneline --after=$After_Date --until=$Until_Date\` ; if [ ! -z \"\$GIT_LOG\" ]; then printf  '\n   * '; realpath \`pwd\` | sed 's|^$CURRENT_PATH/||' ; printf \"\$GIT_LOG\"; fi" >> $Changelog
+
 echo "" >> $Changelog;
 done
 
